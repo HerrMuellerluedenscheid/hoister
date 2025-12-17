4dcf212bd8ab2102957d1d99b9b8cadc9ae01e02
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2025-12-17 08:47:58 +0100

    bugfix: fix credentials missing

--- frontend/src/routes/missing-credentials/+page.svelte
+++ frontend/src/routes/missing-credentials/+page.svelte
@@ -1,14 +1,14 @@
-<div>
-    <h1 class="text-2xl font-bold mb-4">Missing Credentials</h1>
+<div class="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+    <div>
+    <h1 class="text-2xl font-bold mb-4">No Credentials Set</h1>
     <p class="text-gray-700">
         Restart the frontend with the necessary credentials set in the environment variables
     </p>
-    <p>
+    <p class="py-5">
         Required variables:
-        <ul class="list-disc list-inside">
-            <li><code>DOCKER_HOST</code></li>
-            <li><code>DOCKER_CERT_PATH</code> (if using TLS)</li>
-            <li><code>DOCKER_TLS_VERIFY</code> (if using TLS)</li>
-        </ul>
+        <code>
+            HOISTER_AUTH_USERNAME
+        </code> and <code>HOISTER_AUTH_PASSWORD</code>
     </p>
+    </div>
 </div>
