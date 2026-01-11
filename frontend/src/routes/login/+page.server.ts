ffbf87815cb9bb77b2fc5c6433ed8bfda80e7c94
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2026-01-11 12:49:11 +0100

    configure svelte server side via environment variables

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -1,12 +1,12 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+import {config} from "$lib/server/config";
 
 const USERNAME = process.env.HOISTER_AUTH_USERNAME;
 const PASSWORD = process.env.HOISTER_AUTH_PASSWORD;
 
 export const actions = {
   default: async ({ request, cookies }) => {
-    console.debug("logging attempt");
     const data = await request.formData();
     const username = data.get('username');
     const password = data.get('password');
@@ -15,15 +15,14 @@ export const actions = {
     }
 
     if (username === USERNAME && password === PASSWORD) {
-      console.debug("logged in");
       cookies.set('session', 'authenticated', {
         path: '/',
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 60 * 60 * 24 * 7 // 1 week
+        secure: config.useSecureCookies,
+        sameSite: config.cookieSameSite,
+        maxAge: config.sessionMaxAge
       });
-
+      console.debug("logged in");
       throw redirect(303, '/containers');
     }
 
