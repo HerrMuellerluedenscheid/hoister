7976d6d51b108d416ca1bd0399561d84c9b9ba5b
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2026-06-13 17:37:59 +0200

    bump: documentation dependencies

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					items: [{ autogenerate: { directory: 'reference' } }],
 				},
 			],
 			customCss: [
